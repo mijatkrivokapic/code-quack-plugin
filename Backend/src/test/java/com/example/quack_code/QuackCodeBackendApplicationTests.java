19337c89349dfbb752ad30789c19251fec1824ba
Mijat Krivokapić <mijat2004@gmail.com>
2025-12-13 18:07:11 +0100

    Add backend project

---
+++ Backend/src/test/java/com/example/quack_code/QuackCodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quack_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuackCodeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
